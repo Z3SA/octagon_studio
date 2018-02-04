66e5639d904702a9f4990c2227cb506b810e2436
Z3SA <silver.lets-go@yandex.ru>
2018-02-04 20:14:54 +0800

    C# version removed, started development on JavaScript

--- octagon_studio/classes/data/program/Note.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Octagon.Program
-{
-    public class OMSNote
-    {
-        public string Title { get; set; }
-        public string For { get; set; }
-        public string Content { get; set; }
-    }
-}

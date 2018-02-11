334a6cbef7d80463ddceee339f15cb468c14bd78
Z3SA <silver.lets-go@yandex.ru>
2018-02-12 04:22:29 +0800

    Created env for JS development

---
+++ src/electron-starter.js
@@ -0,0 +1,47 @@
+const electron   = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 900,
+        icon: __dirname + "/images/oms_logo-square.ico",
+        title: "Octagon Modmaking Studio",
+        icon: path.join(__dirname, '../images/oms_logo-square.png')
+    });
+
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '/../build/index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+
+    mainWindow.setMenu(null);
+    mainWindow.loadURL(startUrl);
+
+    //mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow == null) {
+        createWindow();
+    }
+});

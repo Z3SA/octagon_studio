f254861d04c8a2d048f868c8a5355740dcd0c400
Z3SA <silver.lets-go@yandex.ru>
2018-02-12 05:31:05 +0800

    Removed default App.js

--- src/windows/App.js
+++ src/windows/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import omsLogo from '../../images/oms_logo.svg';
 
-export default class App extends Component {
+export default class WinMain extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+
+      </main>
     );
   }
 }

aac0648121f41b48f2fbd240850d2096b2c0287d
Z3SA <silver.lets-go@yandex.ru>
2018-02-12 16:51:23 +0800

    Added scss support in project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './windows/App';
+import './index.scss';
+import App from './windows/WinMain';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

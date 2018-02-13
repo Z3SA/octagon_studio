6f3a483cc5148df1276967a67e7ca10a6d5d0b9f
Z3SA <silver.lets-go@yandex.ru>
2018-02-13 18:40:23 +0800

    File restructure

---
+++ src/windows/WinMain/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './WinMain.scss';
+import omsLogo from '../../assets/images/oms_logo.svg';
+
+export default class WinMain extends Component {
+    render() {
+        return (
+            <main>
+                
+            </main>
+        );
+    }
+}

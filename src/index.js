21f3b388b79ee75e67471a6577ab0aa5c5b28290
Z3SA <silver.lets-go@yandex.ru>
2018-02-15 20:53:10 +0800

    Added remote access for FS and ipcRenderer from React

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import './index.scss';
 import App from './windows/WinMain';
 // Importing register service
 import registerServiceWorker from './registerServiceWorker';
-import OMS from './data/app/OMS';
 
 // Link to electron libs for working with OS
 const electron = window.require('electron');
 const fs = electron.remote.require('fs');
+const ipcRenderer = electron.ipcRenderer;
 
 // Rendering whole application
 ReactDOM.render(<App />, document.getElementById('root'));

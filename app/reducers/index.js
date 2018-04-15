ec7834c19aca241fb7d893babaf336ae0fc301ca
Z3SA <silver.lets-go@yandex.ru>
2018-04-15 19:21:31 +0800

    Some changes pulled back for working

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,12 +2,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 import counter from './counter';
-import appMain from './app';
 
 const rootReducer = combineReducers({
     counter,
-    router,
-    appMain
+    router
 });
 
 export default rootReducer;

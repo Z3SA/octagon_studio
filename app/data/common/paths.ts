56053de471857fbd54239eab2e69bdea4a435c0e
Z3SA <silver.lets-go@yandex.ru>
2019-04-05 02:41:42 +0800

    [UPD] Changed paths const to enum

---
+++ app/data/common/paths.ts
@@ -0,0 +1,8 @@
+export enum paths {
+  appData = `${process.env.APPDATA}/Octagon Modmaking Studio`,
+  octagonCfg = 'octagon.oms',
+  langsFolder = '/langs',
+  session = 'session.oms',
+  theme = 'theme.oms',
+  user = '/user.omsdata',
+}

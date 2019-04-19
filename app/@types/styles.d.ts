5b75ba8b794cf5de9a25b864ac8cf6b11d9e48ed
Z3SA <silver.lets-go@yandex.ru>
2019-04-20 04:54:46 +0800

    [ADD] Created global declare for SCSS modules

---
+++ app/@types/styles.d.ts
@@ -0,0 +1,14 @@
+declare module '*.m.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.m.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.m.sass' {
+  const classes: { [key: string]: string };
+  export default classes;
+}

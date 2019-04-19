ab397ae34b187ff025dc28e1864c2d60f442cfba
Z3SA <silver.lets-go@yandex.ru>
2019-04-20 05:46:26 +0800

    [ADD] Added build type near to logo

--- app/modules/main/AppHeader/AppHeader.tsx
+++ app/modules/main/AppHeader/AppHeader.tsx
@@ -4,14 +4,32 @@ import { Layout } from 'antd';
 
 import MainMenu from '../MainMenu';
 import styles from './AppHeader.m.scss';
+import { oms } from '../../../data/data.init';
 
 const { Header } = Layout;
 
 export default class AppHeader extends Component {
   public render() {
+    let buildTypeLabel;
+
+    switch (oms.type) {
+      case 'pre-alpha':
+      case 'alpha':
+        buildTypeLabel = <React.Fragment>&alpha;</React.Fragment>;
+        break;
+      case 'beta':
+        buildTypeLabel = <React.Fragment>&beta;</React.Fragment>;
+        break;
+      case 'dev':
+        buildTypeLabel = 'd';
+        break;
+      default:
+        buildTypeLabel = '';
+    }
+
     return (
       <Header className={styles.AppHeader}>
-        <span className="main-win__logo" />
+        <span className={styles.AppHeader__logo}>{buildTypeLabel}</span>
         <MainMenu />
       </Header>
     );

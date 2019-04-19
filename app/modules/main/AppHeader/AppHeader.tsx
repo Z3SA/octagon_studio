c70a31d8745e0b86004c2083a1082369521387cd
Z3SA <silver.lets-go@yandex.ru>
2019-04-20 05:00:57 +0800

    [UPD] Renamed SCSS of AppHeader to SCSS Module

--- app/modules/main/AppHeader/AppHeader.tsx
+++ app/modules/main/AppHeader/AppHeader.tsx
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 import { Layout } from 'antd';
 
 import MainMenu from '../MainMenu';
-import './AppHeader.scss';
+import styles from './AppHeader.m.scss';
 
 const { Header } = Layout;
 
 export default class AppHeader extends Component {
   public render() {
     return (
-      <Header className="AppHeader">
+      <Header className={styles.AppHeader}>
         <span className="main-win__logo" />
         <MainMenu />
       </Header>

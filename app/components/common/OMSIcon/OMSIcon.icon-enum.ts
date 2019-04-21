b7e546d46b73d2f622d4c88b72ef34d600f6763d
Z3SA <silver.lets-go@yandex.ru>
2019-04-21 17:17:54 +0800

    [ADD] Created component of icon

---
+++ app/components/common/OMSIcon/OMSIcon.icon-enum.ts
@@ -0,0 +1,3 @@
+export enum EOmsIconIconName {
+  settings = 'settings',
+}
--- app/components/common/OMSIcon/OMSIcon.tsx
+++ app/components/common/OMSIcon/OMSIcon.tsx
@@ -1,3 +1,34 @@
-import { PureComponent } from 'react';
+import React, { PureComponent } from 'react';
+// import cx from 'classnames';
 
-export default class OMSIcon extends PureComponent {}
+import styles from './OMSIcon.m.scss';
+import { EOmsIconIconName } from './OMSIcon.icon-enum';
+
+interface IOMSIconProps {
+  icon: EOmsIconIconName;
+  size?: number;
+  color?: string;
+  weight?: 'light' | 'regular' | 'solid';
+}
+
+export default class OMSIcon extends PureComponent<IOMSIconProps> {
+  static defaultProps: IOMSIconProps = {
+    icon: null,
+    size: 24,
+    weight: 'solid',
+  };
+
+  render() {
+    const { icon, size, color, weight } = this.props;
+    return (
+      <span
+        className={`${styles.OMSIcon} ${styles[`OMSIcon--icon-${icon}-${weight}`]}`}
+        style={{
+          width: size,
+          height: size,
+          backgroundColor: color,
+        }}
+      />
+    );
+  }
+}

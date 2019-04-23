c8a40669d8f4aec9cbf7116a5c604bf2be82fbae
Z3SA <silver.lets-go@yandex.ru>
2019-04-24 06:11:06 +0800

    [ADD] Added definitions of icons for window controls

--- app/components/common/OMSIcon/OMSIcon.icon-enum.ts
+++ app/components/common/OMSIcon/OMSIcon.icon-enum.ts
@@ -1,3 +1,7 @@
 export enum EOmsIconIconName {
   settings = 'settings',
+  windowClose = 'window-close',
+  windowMaximize = 'window-maximize',
+  windowMinimize = 'window-minimize',
+  windowRestore = 'window-restore',
 }

66463231a487b8fb14df4d1bd39b8635c445d388
Z3SA <silver.lets-go@yandex.ru>
2019-04-24 13:04:00 +0800

    [ADD] Added click handler for window control

--- app/components/module/main/AppHeaderButton/AppHeaderButton.tsx
+++ app/components/module/main/AppHeaderButton/AppHeaderButton.tsx
@@ -2,10 +2,25 @@ import React, { PureComponent } from 'react';
 
 interface IAppHeaderButtonProps {
   children: any;
+  onClick?: any;
 }
 
 export default class AppHeaderButton extends PureComponent<IAppHeaderButtonProps> {
+  constructor(props: any) {
+    super(props);
+    this.emitClick = this.emitClick.bind(this);
+  }
+
+  /** Emit on click of button */
+  emitClick(): void {
+    this.props.onClick();
+  }
+
   render() {
-    return <button type="button">{this.props.children}</button>;
+    return (
+      <button type="button" onClick={this.emitClick}>
+        {this.props.children}
+      </button>
+    );
   }
 }

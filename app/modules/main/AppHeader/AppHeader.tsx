5af10637fb393673ea797e7d54f179411866e21e
Z3SA <silver.lets-go@yandex.ru>
2019-04-25 19:06:47 +0800

    [DEL] Removed build type definition near to logo

--- app/modules/main/AppHeader/AppHeader.tsx
+++ app/modules/main/AppHeader/AppHeader.tsx
@@ -5,7 +5,6 @@ import Divider from 'antd/lib/divider';
 import electron, { BrowserWindow } from 'electron';
 
 import styles from './AppHeader.m.scss';
-import { oms } from 'data/data.init';
 import OMSIcon from 'components/common/OMSIcon/OMSIcon';
 import { EOmsIconIconName } from 'components/common/OMSIcon/OMSIcon.icon-enum';
 import AppHeaderButton from 'components/module/main/AppHeaderButton/AppHeaderButton';
@@ -49,27 +48,10 @@ export default class AppHeader extends PureComponent<{}, IAppHeaderState> {
   }
 
   public render() {
-    let buildTypeLabel;
-
-    switch (oms.type) {
-      case 'pre-alpha':
-      case 'alpha':
-        buildTypeLabel = <React.Fragment>&alpha;</React.Fragment>;
-        break;
-      case 'beta':
-        buildTypeLabel = <React.Fragment>&beta;</React.Fragment>;
-        break;
-      case 'dev':
-        buildTypeLabel = 'd';
-        break;
-      default:
-        buildTypeLabel = '';
-    }
-
     return (
       <Header className={styles.AppHeader}>
         <div className={styles.AppHeader__left}>
-          <span className={styles.AppHeader__logo}>{buildTypeLabel}</span>
+          <span className={styles.AppHeader__logo} />
         </div>
 
         <div className={styles.AppHeader__nav}>&nbsp;</div>

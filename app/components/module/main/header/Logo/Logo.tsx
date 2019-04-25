9f3f62d40cbfc0435ef704fdafb4d46854871ffd
Z3SA <silver.lets-go@yandex.ru>
2019-04-25 22:40:18 +0800

    [ADD] Created external open of Github

--- app/components/module/main/header/Logo/Logo.tsx
+++ app/components/module/main/header/Logo/Logo.tsx
@@ -1,10 +1,12 @@
 import React, { PureComponent } from 'react';
+import { shell } from 'electron';
 
 import Menu from 'antd/lib/menu';
 import Dropdown from 'antd/lib/dropdown';
 
 import styles from './Logo.m.scss';
 import AboutAppModal from '../../modals/AboutAppModal/AboutAppModal';
+import { APP_CONSTS } from 'data/utils/AppConsts.enum';
 
 export default class Logo extends PureComponent {
   menu = (
@@ -15,8 +17,17 @@ export default class Logo extends PureComponent {
       <Menu.Item key="check-updates" disabled={true}>
         Проверить обновления
       </Menu.Item>
+      <Menu.Item key="open-repo" onClick={this.openRepo}>
+        Открыть репозиторий
+      </Menu.Item>
     </Menu>
   );
+
+  /** Open GitHub repo in default browser */
+  openRepo(): void {
+    shell.openExternal(APP_CONSTS.GITHUB_LINK);
+  }
+
   render() {
     return (
       <Dropdown overlay={this.menu} trigger={['click']}>
---
+++ app/data/utils/AppConsts.enum.ts
@@ -0,0 +1,3 @@
+export enum APP_CONSTS {
+  GITHUB_LINK = 'https://github.com/Z3SA/octagon_studio',
+}

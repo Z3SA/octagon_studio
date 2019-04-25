e3741ba4d2d19be0ef43d23c416f455aa8381d1f
Z3SA <silver.lets-go@yandex.ru>
2019-04-25 19:47:30 +0800

    [ADD] Added dropdown for logo in app header

--- app/components/module/main/header/Logo/Logo.tsx
+++ app/components/module/main/header/Logo/Logo.tsx
@@ -1,9 +1,21 @@
 import React, { PureComponent } from 'react';
 
+import Menu from 'antd/lib/menu';
+import Dropdown from 'antd/lib/dropdown';
+
 import styles from './Logo.m.scss';
 
 export default class Logo extends PureComponent {
+  menu = (
+    <Menu>
+      <Menu.Item key="about-app">О программе</Menu.Item>
+    </Menu>
+  );
   render() {
-    return <span className={styles.Logo} />;
+    return (
+      <Dropdown overlay={this.menu} trigger={['click']}>
+        <span className={styles.Logo} />
+      </Dropdown>
+    );
   }
 }

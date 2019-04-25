47ffde3323b9527cf9d2b0349f004178f91d1cd3
Z3SA <silver.lets-go@yandex.ru>
2019-04-25 19:15:37 +0800

    [UPD] Rewrited component of main menu

--- app/components/module/main/Menu/MenuSeparator.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-// Separator for menus
-export default class MenuSeparator extends Component {
-  render() {
-    return <li className="menu__separator" />;
-  }
-}

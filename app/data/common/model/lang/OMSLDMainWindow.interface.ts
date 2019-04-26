350bc46d40a86be204fff96e38f5d703fda1366c
Z3SA <silver.lets-go@yandex.ru>
2019-04-26 23:51:27 +0800

    [ADD] Added interface of language pack of about app modal

--- app/data/common/model/lang/OMSLDMainWindow.interface.ts
+++ app/data/common/model/lang/OMSLDMainWindow.interface.ts
@@ -1,6 +1,10 @@
 import IOMSLDLogoMenu from './OMSLDLogoMenu.interface';
+import IOMSLDAboutAppModal from './OMSLDAboutAppModal.interface';
 
 export default interface IOMSLDMainWindow {
   /** Dropdown of menu in app header */
   LOGO_MENU: IOMSLDLogoMenu;
+
+  /** Modal "About app" */
+  ABOUT_APP: IOMSLDAboutAppModal;
 }

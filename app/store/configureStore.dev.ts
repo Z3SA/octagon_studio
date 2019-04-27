acd5bcb98a4ed062d4d538640fcf6f5cf5bdf642
Z3SA <silver.lets-go@yandex.ru>
2019-04-28 04:10:28 +0800

    [UPD] Turned off enhancers, middlewares, thunks

--- app/store/configureStore.dev.ts
+++ app/store/configureStore.dev.ts
@@ -1,8 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, compose } from 'redux';
 import { createHashHistory } from 'history';
-import { routerMiddleware, routerActions } from 'connected-react-router';
-import { createLogger } from 'redux-logger';
+import { routerActions } from 'connected-react-router';
 import createRootReducer from './reducers';
 import * as counterActions from './actions/counter';
 
@@ -12,26 +10,26 @@ const rootReducer = createRootReducer(history);
 
 const configureStore = initialState => {
   // Redux Configuration
-  const middleware = [];
-  const enhancers = [];
+  // const middleware = [];
+  // const enhancers = [];
 
   // Thunk Middleware
-  middleware.push(thunk);
+  // middleware.push(thunk);
 
   // Logging Middleware
-  const logger = createLogger({
-    level: 'info',
-    collapsed: true,
-  });
+  // const logger = createLogger({
+  //   level: 'info',
+  //   collapsed: true,
+  // });
 
   // Skip redux logs in console during the tests
-  if (process.env.NODE_ENV !== 'test') {
-    middleware.push(logger);
-  }
+  // if (process.env.NODE_ENV !== 'test') {
+  //   middleware.push(logger);
+  // }
 
   // Router Middleware
-  const router = routerMiddleware(history);
-  middleware.push(router);
+  // const router = routerMiddleware(history);
+  // middleware.push(router);
 
   // Redux DevTools Configuration
   const actionCreators = {
@@ -40,20 +38,21 @@ const configureStore = initialState => {
   };
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
   /* eslint-disable no-underscore-dangle */
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Options: http://extension.remotedev.io/docs/API/Arguments.html
-        actionCreators,
-      })
-    : compose;
+  // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  //   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+  //       // Options: http://extension.remotedev.io/docs/API/Arguments.html
+  //       actionCreators,
+  //     })
+  //   : compose;
   /* eslint-enable no-underscore-dangle */
 
   // Apply Middleware & Compose Enhancers
-  enhancers.push(applyMiddleware(...middleware));
-  const enhancer = composeEnhancers(...enhancers);
+  // enhancers.push(applyMiddleware(...middleware));
+  // const enhancer = composeEnhancers(...enhancers);
 
   // Create Store
-  const store = createStore(rootReducer, initialState, enhancer);
+  // const store = createStore(rootReducer, initialState, enhancer);
+  const store = createStore(rootReducer, initialState);
 
   if (module.hot) {
     module.hot.accept(

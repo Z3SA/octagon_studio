546877818be46f1aa489889b63afca2b1a394cdf
Z3SA <silver.lets-go@yandex.ru>
2019-05-07 18:30:01 +0800

    [ADD] Created reducer of main window environment

--- app/store/model/main.interface.ts
+++ app/store/model/main.interface.ts
@@ -1,5 +1,7 @@
 import { IStoreMainModals } from './main/modals.interface';
+import { IStoreMainEnvironment } from './main/environment.interface';
 
 export interface IStoreMain {
   modals: IStoreMainModals;
+  environment: IStoreMainEnvironment;
 }

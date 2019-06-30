117de66f690086122648c82da2846973ef290dc9
Z3SA <silver.lets-go@yandex.ru>
2019-06-30 21:38:50 +0800

    [ADD] Created layout of statusbar

--- app/modules/main/StatusBar/StatusBar.tsx
+++ app/modules/main/StatusBar/StatusBar.tsx
@@ -2,10 +2,51 @@ import React, { PureComponent } from 'react';
 
 import Layout from 'antd/lib/layout/layout';
 
+import styles from './StatusBar.m.scss';
+import generateMixClass from 'components/utils/mixClass';
+import IOMSLDStatusbar from 'data/common/model/lang/main_window/OMSLDStatusbar.interface';
+import { TranslateContext } from 'modules/global/TranslateContext';
+
 const { Footer } = Layout;
 
 export default class StatusBar extends PureComponent {
+  static contextType = TranslateContext;
+
+  lang: IOMSLDStatusbar = this.context.MAIN_WINDOW.STATUSBAR;
+
   render() {
-    return <Footer />;
+    return (
+      <Footer className={styles.StatusBar}>
+        <div
+          className={generateMixClass([
+            {
+              name: styles.StatusBar__side,
+              condition: true,
+            },
+            {
+              name: styles['StatusBar__side--left'],
+              condition: true,
+            },
+          ])}
+        >
+          <div className={styles.StatusBar__cell}>{this.lang.PROGRAM_STATUS.START}</div>
+        </div>
+
+        <div
+          className={generateMixClass([
+            {
+              name: styles.StatusBar__side,
+              condition: true,
+            },
+            {
+              name: styles['StatusBar__side--right'],
+              condition: true,
+            },
+          ])}
+        >
+          <div className={styles.StatusBar__cell}>Mk 0 Insiders (v0.6.0)</div>
+        </div>
+      </Footer>
+    );
   }
 }

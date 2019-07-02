7254e0ab645a09e95a830f07f54994ebe00f66f2
Z3SA <silver.lets-go@yandex.ru>
2019-07-02 22:06:52 +0800

    [UPD] Refactored app data paths with usage of enum of file extensions

--- app/data/common/appData.ts
+++ app/data/common/appData.ts
@@ -1,13 +1,15 @@
+import { fileExtensions } from './fileExtensions.enum';
+
 /** Folders and files in App Data folder of editor */
 const appData = {
   folder: `${process.env.APPDATA}/Octagon Modmaking Studio/Data`,
-  cfg: 'octagon.oms',
+  cfg: `octagon.${fileExtensions.CFG}`,
   langsFolder: '/langs',
-  langsMeta: 'langsmeta.oms',
-  session: 'session.oms',
-  windowSession: 'window_session.oms',
-  theme: 'theme.oms',
-  user: 'user.oms',
+  langsMeta: `langsmeta.${fileExtensions.CFG}`,
+  session: `session.${fileExtensions.CFG}`,
+  windowSession: `window_session.${fileExtensions.CFG}`,
+  theme: `theme.${fileExtensions.CFG}`,
+  user: `user.${fileExtensions.CFG}`,
 };
 
 export default appData;

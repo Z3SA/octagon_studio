062aaf554dc1a37ee3edabf4a4d673d9ccc7a7e3
Z3SA <silver.lets-go@yandex.ru>
2019-07-06 14:48:31 +0800

    [ADD] Created action and reducer for language list

--- app/store/reducers/app/ui.ts
+++ app/store/reducers/app/ui.ts
@@ -1,17 +1,31 @@
-import { SET_LANGUAGE } from 'store/actions/app/language';
-import { ISetLanguageAction } from 'store/actions/app/language';
+import { SET_LANGUAGE, SET_LANG_LIST, ISetLangListAction } from 'store/actions/app/ui';
+import { ISetLanguageAction } from 'store/actions/app/ui';
 import IStoreAppUI from 'store/model/app/ui.interface';
 
 const initialState: IStoreAppUI = {
   language: 'en',
+  langList: [],
 };
 
-export function uiReducer(state = initialState, action: ISetLanguageAction): IStoreAppUI {
+export function uiReducer(
+  state = initialState,
+  action: ISetLanguageAction | ISetLangListAction
+): IStoreAppUI {
   switch (action.type) {
     case SET_LANGUAGE:
       return {
         ...state,
         language: action.lang,
+      };
+    case SET_LANG_LIST:
+      return {
+        ...state,
+        langList: [
+          ...state.langList,
+          ...action.langList.filter(
+            i => state.langList.map(({ abbr }) => abbr).indexOf(i.abbr) === -1
+          ),
+        ],
       };
     default:
       return state;

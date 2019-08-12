fb460853d924edf9e2ce9133fa9b45b47bd4b43b
Z3SA <silver.lets-go@yandex.ru>
2019-08-13 02:15:24 +0800

    [UPD] Try to typedef translate context

--- app/modules/global/TranslateContext.ts
+++ app/modules/global/TranslateContext.ts
@@ -1,4 +1,6 @@
 import React from 'react';
 
+import { IOMSLD } from 'data/common/model/lang';
+
 /** Context of translate */
-export const TranslateContext = React.createContext({});
+export const TranslateContext = React.createContext({} as IOMSLD);
